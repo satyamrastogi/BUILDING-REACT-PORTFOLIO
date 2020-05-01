0fcb62e4a8a2c150b4cef13099b6b5d3b8b432c6
satyamrastogi <satyam.mnnit97@gmail.com>
2020-05-01 14:02:41 +0530

    add info pop up button in card to reac information

---
+++ src/components/InfoPopUpButton.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function InfoPopUpButton(props) {
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState("paper");
+
+  const handleClickOpen = scrollType => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <Button size="small" onClick={handleClickOpen("paper")}>Read More</Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">{props.title}</DialogTitle>
+        <DialogContent dividers={scroll === "paper"}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            {props.content}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button  onClick={handleClose} color="dark">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/components/blog/Cards.js
+++ src/components/blog/Cards.js
@@ -6,7 +6,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import InfoPopUpButton from '../InfoPopUpButton'
 
 class Cards extends React.Component {  
   
@@ -31,12 +31,10 @@ class Cards extends React.Component {
             </CardContent>
           </CardActionArea>
           <CardActions>
-            <Button size="small" color="primary">
+            <Button size="small" color="dark">
               Share
             </Button>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
+            <InfoPopUpButton title={this.props.item.title} content={this.props.item.intro}/>
           </CardActions>
         </Card>
       );
